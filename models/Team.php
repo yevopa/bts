f6c961eea8dd8aae5226c2fca7d461e0e3e52a72
artroot <art90com@gmail.com>
2018-02-19 22:31:33 +0200

    Add basic Team Model & CRUD

---
+++ models/Team.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "team".
+ *
+ * @property int $id
+ */
+class Team extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'team';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+        ];
+    }
+}
